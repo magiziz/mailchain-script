aa76fc4de8b965b361c9e60a0e7bf1ddc902f888
Magomed Khamidov <53529533+KosmosKey@users.noreply.github.com>
2023-04-04 19:38:23 +0100

    🧼 Created function to send mail chain email

--- index.ts
+++ index.ts
@@ -1,17 +1,58 @@
-import { resolveAddress } from "@mailchain/sdk/internal";
+import { getPrivateMessagingKey } from "@mailchain/sdk/internal";
+import { KeyRing } from "@mailchain/keyring";
+import { MailSender } from "@mailchain/sdk/internal";
+import { ED25519PrivateKey } from "@mailchain/crypto";
+import { decodeHex } from "@mailchain/encoding";
+import { encodeHex } from "@mailchain/encoding";
+import "dotenv/config";
+
+const createPrivateMessagingKey = async () => {
+  const keyRing = KeyRing.fromSecretRecoveryPhrase(
+    process.env.SECRET_KEY_RECOVERY_PHRASE as string
+  );
+
+  const mailchainAddress = process.env.MAILCHAIN_ADDRESS as string;
+
+  const { data: privateMessagingKey, error: getPrivateMessagingKeyError } =
+    await getPrivateMessagingKey(mailchainAddress, keyRing);
+
+  if (getPrivateMessagingKeyError) throw getPrivateMessagingKeyError;
+
+  return encodeHex(privateMessagingKey.publicKey.bytes);
+};
 
 const bootstrap = async () => {
-  console.log("should throw error");
-  const address =
-    "0xbb56FbD7A2caC3e4C17936027102344127b7a112@ethereum.mailchain.com";
-  const { error: resolveAddressError } = await resolveAddress(address);
+  const privateMessagingKeyBytes = await createPrivateMessagingKey();
 
-  if (resolveAddressError) {
-    const { type, message } = resolveAddressError;
-    console.warn(`ERROR check address - ${address} - ${type} - ${message}`);
+  console.log('✅ private messaging key bytes created:', privateMessagingKeyBytes)
+
+  const recoveredPrivateMessagingKey = ED25519PrivateKey.fromSeed(
+    decodeHex(privateMessagingKeyBytes)
+  );
+
+  console.log('✅ recovered private messaging key:', recoveredPrivateMessagingKey.bytes)
+
+  const mailchainAddress = process.env.MAILCHAIN_ADDRESS as string;
+
+  const { data: sentMail, error: sendMailError } =
+    await MailSender.fromSenderMessagingKey(
+      recoveredPrivateMessagingKey
+    ).sendMail({
+      from: mailchainAddress,
+      to: [`0x8ebdc1c3c2d4e0b77831f654cdde5e9e5b0909e5@ethereum.mailchain.com`],
+      subject: "My first message",
+      content: {
+        text: "Hello Mailchain 👋",
+        html: "<p>Hello Mailchain 👋</p>",
+      },
+    });
+
+    if (sendMailError) {
+    console.log('❌ send mail error:',sendMailError)
+    throw sendMailError;
   }
 
-  console.log(`${address} is reachable.`);
+  console.log(`✅ Message sent successfully: ${sentMail}`);
 };
 
 bootstrap();
