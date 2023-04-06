d639ffee91f50a8fb2cc60b4a9fd8ce633680b63
Rob De Feo <robdefeo@gmail.com>
2023-04-06 14:39:33 +0200

    chore: clean up imports

--- index.ts
+++ index.ts
@@ -1,25 +1,5 @@
-import { getPrivateMessagingKey, privateMessagingKeyFromHex } from "@mailchain/sdk/internal";
-import { KeyRing } from "@mailchain/keyring";
-import { MailSender } from "@mailchain/sdk/internal";
-import { ED25519PrivateKey } from "@mailchain/crypto";
-import { decodeHex } from "@mailchain/encoding";
-import { encodeHex } from "@mailchain/encoding";
+import { MailSender, privateMessagingKeyFromHex } from "@mailchain/sdk/internal";
 import "dotenv/config";
-
-const createPrivateMessagingKey = async () => {
-  const keyRing = KeyRing.fromSecretRecoveryPhrase(
-    process.env.SECRET_KEY_RECOVERY_PHRASE as string
-  );
-
-  const mailchainAddress = process.env.MAILCHAIN_ADDRESS as string;
-
-  const { data: privateMessagingKey, error: getPrivateMessagingKeyError } =
-    await getPrivateMessagingKey(mailchainAddress, keyRing);
-
-  if (getPrivateMessagingKeyError) throw getPrivateMessagingKeyError;
-
-  return encodeHex(privateMessagingKey.publicKey.bytes);
-};
 
 const createMailSender = () => {
   const privateMessagingKey = privateMessagingKeyFromHex(process.env.ADDRESS_PRIVATE_MESSAGING_KEY as string);
